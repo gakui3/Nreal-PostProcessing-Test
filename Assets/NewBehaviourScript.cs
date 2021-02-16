d998c84308289e5b936a46e951eb0b318f5a38c0
kiriigaku <gaku.kirii@gmail.com>
2021-02-16 17:39:29 +0900

    firast commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NRKernal.NRExamples;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    VideoCapture2LocalExample controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(VideoCapture());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator VideoCapture()
+    {
+        yield return new WaitForSeconds(5f);
+        controller.StartVideoCapture();
+        yield return new WaitForSeconds(10f);
+        controller.StopVideoCapture();
+        yield return null;
+    }
+}
